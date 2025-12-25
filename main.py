0540b5fdd39e67df34de31fbf836efd4f7728eab
Elias <3elias3rd@gmail.com>
2025-12-25 12:59:54 +0400

    Added comments for clarity

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ from spacy.util import compounding, minibatch
 
 from training_data import TRAIN_DATA
 
+# Create path to model
 MODEL_DIR = Path("model")/"intent_bot"
 
+# Funtion to create the pipeline with textcat componet and all 4 labels.
 def create_nlp_pipeline():
     nlp = spacy.load("en")
 
     textcat = nlp.add_pipe("textcat", last=True)
 
     labels = ["farewell", "greeting", "location", "pricing"]
+
+    # Add each label to the textcat component
     for label in labels:
         textcat.add_label(label)
     return nlp
 
+# Function takes the pipeline and runs text that has been converted into a doc object and makes predictions based on training.
 def train(nlp, training_data, n_iter=20):
     examples = []
     for text, ann in training_data:
@@ -28,14 +33,20 @@ def train(nlp, training_data, n_iter=20):
 
     optimizer = nlp.initialize(lambda:examples)
 
+    # For each run through the training data
     for epoch in range(n_iter):
         random.shuffle(examples)
         losses = {}
+
+        # Create smaller beatches from the list of example that increase in size slowly with each batch for 4 Example objects to 32 Example objects.
         batches = minibatch(examples, size=compounding(4.0, 32.0, 1.001))
         for batch in batches:
             nlp.update(batch, sgd=optimizer, losses=losses)
-    print(f"Iteration: {epoch+1} | Loss: {losses.get('textcat', 0.0):.3f}")
 
+        # Print Losses after each run through the data. (losses should go down over time)
+        print(f"Iteration: {epoch+1} | Loss: {losses.get('textcat', 0.0):.3f}")
+
+    # Upload the model to the directory. Make sure MODEL_DIR exists and create if necessary
     MODEL_DIR.mkdir(parents=True, exist_ok=True)
     nlp.to_disk(MODEL_DIR)
     print(f"Model trained and uploaded to {MODEL_DIR.resolve()}")
