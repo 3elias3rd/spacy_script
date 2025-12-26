2d76abdaecd153655fbab7faeab848497e100377
Elias <3elias3rd@gmail.com>
2025-12-26 15:39:48 +0400

    Added inference.py to Load trained spacy model from disk, take user text to run it through the model and return predicted intents,

---
+++ Inference.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+from typing import Dict, Any
+import spacy
+
+MODEL_DIR = Path("models")/"nlp_intent_model"
+FALLBACK_THRESHOLD = 0.6
+
+class IntentClassifier:
+    def __init__(self, model_dir: Path=MODEL_DIR):
+        if not model_dir.exists():
+            raise FileExistsError(f"Model {model_dir}has not been trained and uploaded to the directory.")
+        
+        self.nlp =spacy.load(model_dir)
+
+        if "textcat" not in self.nlp.pipe_names:
+            raise ValueError(f"{model_dir} has no textcat component.")
+        
+    def predict(self, text: str) -> Dict[str,Any]:
+        if not isinstance(text, str) or not text.strip():
+            return{"intent": None, "confidence": 0.0, "fallback": True}
+        
+        doc = self.nlp(text)
+        cats = doc.cats
+        top_label = max(cats, key=cats.get)
+
+        confidence = float(cats[top_label])
+        fallback = confidence < FALLBACK_THRESHOLD
+
+        return {"intent": top_label, "confidence": confidence, "fallback": fallback}
+    
